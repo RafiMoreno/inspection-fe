987d8b16c3ccc3fd4aad1938ca90d4211d8191e2
RafiMoreno <ahmadarrafi.m@gmail.com>
2025-02-26 16:00:32 +0700

    feat: use env variables for api url

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const apiURL = import.meta.env.VITE_API_URL
+
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: apiURL,
   headers: {
     "Content-Type": "multipart/form-data",
   },
